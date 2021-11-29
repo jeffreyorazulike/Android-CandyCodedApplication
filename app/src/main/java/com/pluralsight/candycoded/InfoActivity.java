51ae0545bd7a6f06d8d66a46dabd53ac5fb796a1
Jeffrey Orazulike <chukwudumebiorazulike@gmail.com>
2021-11-29 03:14:44 +0100

    Finished Pluralsight Project

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,5 +1,8 @@
 package com.pluralsight.candycoded;
 
+
+import android.content.Intent;
+import android.view.View;
 import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.net.Uri;
@@ -11,6 +14,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
+
+import com.squareup.picasso.Picasso;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -31,9 +36,22 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
     // ***
+    public void createMapIntent(View view){
+        Uri uri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+    }
 
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view) {
+      Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+      phoneIntent.setData(Uri.parse("tel:0123456789"));
+      startActivity(phoneIntent);
+    }
 
 }
